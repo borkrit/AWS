c651479761c2d86878f317a8bd431b7b2d97f4c8
Vladyslav Pohorilyi <pogoriliy15@gmail.com>
2024-04-03 12:05:24 +0200

    Contact

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -5,7 +5,7 @@ export const metadata = {
     openGraph: {
         title: 'Contact',
         description: 'Contact page',
-        images:'https://dev.gamblerslab.com/public/img/logo-desktop.png',
+        images:'https://i0.wp.com/weckwerthblog.wpcomstaging.com/wp-content/uploads/2023/09/DSC04912.jpg?fit=2048%2C1153&ssl=1',
     },
 };
 
