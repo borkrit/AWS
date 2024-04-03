20b40cca58c24977d70710b8af8547be32aacfe3
Vladyslav Pohorilyi <pogoriliy15@gmail.com>
2024-04-03 12:08:30 +0200

    Contact

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -5,7 +5,11 @@ export const metadata = {
     openGraph: {
         title: 'Contact',
         description: 'Contact page',
-        images:'https://i0.wp.com/weckwerthblog.wpcomstaging.com/wp-content/uploads/2023/09/DSC04912.jpg?fit=2048%2C1153&ssl=1',
+        images:[{
+           url: 'https://i0.wp.com/weckwerthblog.wpcomstaging.com/wp-content/uploads/2023/09/DSC04912.jpg?fit=2048%2C1153&ssl=1',
+            width: 800,
+            height: 600,
+        }]
     },
 };
 
